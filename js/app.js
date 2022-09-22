cf6f6e11f34ac81a79edc3ea4aeddf8bc5262dff
Eric Wajnrajch <ericwaje03@gmail.com>
2022-09-22 16:46:58 -0300

    diseñando cotizador rus

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/* const input = document.getElementById('input');
+const check = document.getElementById('check');
+
+const handleCheckFocus = () => {
+    if (check.classList.contains('invisible')) {
+        check.classList.remove('invisible');
+        check.classList.add('visible');
+    }
+};
+
+const handleCheckBlur = () => {
+    if (check.classList.contains('visible')) {
+        check.classList.remove('visible');
+        check.classList.add('invisible');
+    }
+};
+
+const start = () => {
+    input.addEventListener('focus', handleCheckFocus);
+    input.addEventListener('blur', handleCheckBlur);
+};
+
+window.onload = start; */
